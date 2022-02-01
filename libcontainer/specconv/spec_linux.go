dpttk/runc

commit e298c2defcd6f8a5dae533ca8f392b0e4fb1682a

libct/spec: replace isValidName regex with a function
Also, add a simple test and a benchmark (just out of sheer curiosity).

Benchmark results:

name           old time/op  new time/op  delta
IsValidName-4   540ns ± 3%    45ns ± 1%  -91.76%  (p=0.008 n=5+5)

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

libcontainer/specconv/spec_linux.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -411,8 +410,21 @@ func createLibcontainerMount(cwd string, m specs.Mount) (*configs.Mount, error) 
 	return mnt, nil
 }
 
-// systemd property name check: latin letters only, at least 3 of them
-var isValidName = regexp.MustCompile(`^[a-zA-Z]{3,}$`).MatchString
+// isValidName checks if systemd property name is valid. It should consists
+// of latin letters only, and have least 3 of them.
+func isValidName(s string) bool {
+	if len(s) < 3 {
+		return false
+	}
+	// Check ASCII characters rather than Unicode runes.
+	for _, ch := range s {
+		if (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') {
+			continue
+		}
+		return false
+	}
+	return true
+}
 
 // Some systemd properties are documented as having "Sec" suffix
 // (e.g. TimeoutStopSec) but are expected to have "USec" suffix

libcontainer/specconv/spec_linux_test.go
@@ -762,6 +762,39 @@ func TestInitSystemdProps(t *testing.T) {
 	}
 }
 
+func TestIsValidName(t *testing.T) {
+	testCases := []struct {
+		in    string
+		valid bool
+	}{
+		{"", false},   // too short
+		{"xx", false}, // too short
+		{"xxx", true},
+		{"someValidName", true},
+		{"A name", false},  // space
+		{"3335", false},    // numbers
+		{"Name1", false},   // numbers
+		{"Кир", false},     // non-ascii
+		{"მადლობა", false}, // non-ascii
+		{"合い言葉", false},    // non-ascii
+	}
+
+	for _, tc := range testCases {
+		valid := isValidName(tc.in)
+		if valid != tc.valid {
+			t.Errorf("case %q: expected %v, got %v", tc.in, tc.valid, valid)
+		}
+	}
+}
+
+func BenchmarkIsValidName(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, s := range []string{"", "xx", "xxx", "someValidName", "A name", "Кир", "მადლობა", "合い言葉"} {
+			_ = isValidName(s)
+		}
+	}
+}
+
 func TestNullProcess(t *testing.T) {
 	spec := Example()
 	spec.Process = nil

commit 47adc7d84533721de9b456dda6e65752871b84a9

libct/specconv: improve checkPropertyName
Commit e298c2def replaced a regular expression with code checking the
characters. Despite what the comment said about ASCII, the check was
performed rune by rune, not byte by byte.

Note the check was still correct, basically comparing int32's, but the
byte by byte way is a tad faster and more straightforward. The change
also fixes the issue of a misleading comment.

Benchmark before/after shows a modest improvement:

name                 old time/op    new time/op    delta
CheckPropertyName-4     164ns ± 2%     123ns ± 2%  -24.73%  (p=0.029 n=4+4)

name                 old alloc/op   new alloc/op   delta
CheckPropertyName-4     96.0B ± 0%     64.0B ± 0%  -33.33%  (p=0.029 n=4+4)

name                 old allocs/op  new allocs/op  delta
CheckPropertyName-4      6.00 ± 0%      4.00 ± 0%  -33.33%  (p=0.029 n=4+4)

Fixes: e298c2def
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

libcontainer/specconv/spec_linux.go
@@ -538,8 +538,10 @@ func checkPropertyName(s string) error {
 	if len(s) < 3 {
 		return errors.New("too short")
 	}
-	// Check ASCII characters rather than Unicode runes.
-	for _, ch := range s {
+	// Check ASCII characters rather than Unicode runes,
+	// so we have to use indexes rather than range.
+	for i := 0; i < len(s); i++ {
+		ch := s[i]
 		if (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') {
 			continue
 		}